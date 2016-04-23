059f58521e2b66a07badb084b10b8de53e6b707f
Jvoi <Jvoi@itu.dk>
2016-04-23 21:45:33 +0200

    Add working weather implementation with a bit more data

--- api.py
+++ api.py
@@ -5,44 +5,39 @@ A module for interacting with online APIs
 
 import json
 import httplib
-import googlemaps
-#import android
+#http://api.geonames.org/findNearByWeatherJSON?lat=43&lng=-2&username=demo
+#http://forecast.weather.gov/MapClick.php?lat=38.4247341&lon=-86.9624086&FcstType=json
 
-droid = android.Android()
+
+geo_baseurl = r"api.geonames.org"
+f_baseurl = r"forecast.weather.gov"
+
 owm_key = "fee04268dd271d32dbdefe59fe85f13b"
 owm_baseurl = r"api.openweathermap.org"
-googleKey = "AIzaSyAEe6adJXOA-GtGk9_HZgJMfTzC9TIAoyc"
-googleClient = googlemaps.Client(key=googleKey)
+
+
 
 def loc_weather(lat, lon):
     # Request JSON from OWM
-    conn = httplib.HTTPConnection(owm_baseurl)
-    req = "/data/2.5/weather?lat=" + str(lat) + "&lon=" + str(lon) + "&APPID=fee04268dd271d32dbdefe59fe85f13b"
+    conn = httplib.HTTPConnection(geo_baseurl)
+    req = r"/findNearByWeatherJSON?lat=" + str(lat) r"&lng=" + str(lon) + r"&username=demo"
+    #req = r"/data/2.5/weather?lat=" + str(lat) + "&lon=" + str(lon) + "&APPID=" + owm_key
+    #req = r"/MapClick.php?lat="+str(lat)+r"&lon="+str(lon)+r"&FcstType=json"
     conn.request("GET", req, "")
     res = conn.getresponse()
     body = res.read()
+    print body
     # Turn json into dictionary
-    dict = json.loads(body)
+    try:
+        dict = json.loads(body)
+    except:
+        dict = None
     return dict
-
-def get_location():
-    droid.startLocating()
-    event=droid.eventWaitFor('location', 50000)
-    while 1:
-        try:
-            provider = event.result['data']['gps']['provider']
-            if provider == 'gps':
-                lat = str(event['data']['gps']['latitude'])
-                lon = str(event['data']['gps']['latitude'])
-                return (lat,lon)
-            else: continue
-        except KeyError:
-            continue
     
 def httpTest():
     url = "api.openweathermap.org"
     body = "Request failed."
     conn = httplib.HTTPConnection(url)
-    conn.request("GET", "/data/2.5/weather?zip=2500,dk&APPID=fee04268dd271d32dbdefe59fe85f13b", body)
+    conn.request("GET", "/data/2.5/weather?zip=2500,dk&APPID="+owm_key, body)
     res = conn.getresponse()
     print res.status, res.reason
--- main.py
+++ main.py
@@ -6,6 +6,8 @@ This is a temporary script file.
 """
 
 import kivy
+import gps
+from time import sleep
 
 kivy.require('1.8.0')
 
@@ -13,8 +15,11 @@ from kivy.garden.mapview import MapView
 from kivy.app import App
 
 class MapViewApp(App):
+    
     def build(self):
-        mapview = MapView(zoom=11, lat=50.6394, lon=3.057)
+        gps.start_tracking()
+        sleep(0.2)
+        mapview = MapView(zoom=11, lat=gps.get_lat, lon=gps.get_lon)
         return mapview
 
 MapViewApp().run()
