98bf4fb584fb4fbd93cf0c0c1aff15d33f32d007
Jvoi <Jvoi@itu.dk>
2016-04-24 09:10:53 +0200

    Update weather API
    Probably not going to expand this module further for now.

--- api.py
+++ api.py
@@ -9,21 +9,24 @@ import httplib
 #http://forecast.weather.gov/MapClick.php?lat=38.4247341&lon=-86.9624086&FcstType=json
 
 
-geo_baseurl = r"api.geonames.org"
-f_baseurl = r"forecast.weather.gov"
+#geo_baseurl = r"api.geonames.org"
+#f_baseurl = r"forecast.weather.gov"
 
 owm_key = "fee04268dd271d32dbdefe59fe85f13b"
 owm_baseurl = r"api.openweathermap.org"
 
-
+#noaa_baseurl = r"ncdc.noaa.gov"
+#noaa_key = r"zEYCMRLbHZjDKmMEPdJNdlxvtNoW"
 
 def loc_weather(lat, lon):
     # Request JSON from OWM
-    conn = httplib.HTTPConnection(geo_baseurl)
-    req = r"/findNearByWeatherJSON?lat=" + str(lat) + r"&lng=" + str(lon) + r"&username=demo"
-    #req = r"/data/2.5/weather?lat=" + str(lat) + "&lon=" + str(lon) + "&APPID=" + owm_key
+    conn = httplib.HTTPConnection(owm_baseurl)
+    #req = r"/findNearByWeatherJSON?lat=" + str(lat) + r"&lng=" + str(lon) + r"&username=demo"
+    #req = r""
+    
+    req = r"/data/2.5/weather?lat=" + str(lat) + "&lon=" + str(lon) + "&APPID=" + owm_key
     #req = r"/MapClick.php?lat="+str(lat)+r"&lon="+str(lon)+r"&FcstType=json"
-    conn.request("GET", req, "")
+    conn.request("GET", req, "") #, "token:"+noaa_key)
     res = conn.getresponse()
     body = res.read()
     print body
@@ -38,6 +41,13 @@ def httpTest():
     url = "api.openweathermap.org"
     body = "Request failed."
     conn = httplib.HTTPConnection(url)
-    conn.request("GET", "/data/2.5/weather?zip=2500,dk&APPID="+owm_key, body)
+    conn.request("GET", "/data/2.5/weather?zip=2300,dk&APPID="+owm_key, body)
     res = conn.getresponse()
+    body = res.read()
     print res.status, res.reason
+    print body
+#tests
+print "test 1"
+httpTest()
+from time import sleep
+sleep(3)
